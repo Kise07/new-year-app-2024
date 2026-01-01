c964ac58ba54beb204901fdfee30b6e4382a76f5
kise <kaiazmi1412@gmail.com>
2026-01-01 22:59:30 +0530

    feat: frontend added and init component created!

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,9 @@
+
+export function CreateTodo() {
+  return <div>
+    <input type="text" placeholder="title" />
+    <input type="text" plcareholder="description" />
+
+    <button>Add a todo</button>
+  </div>
+}
